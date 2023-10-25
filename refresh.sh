ad34a295fdc8bebe427b0d9e55fd3a7db2c90f27
Elliot_IA <alexa818@umn.edu>
2023-10-25 09:48:56 -0500

    hey

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,7 @@
 echo ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 echo beginning program refresh...
 git pull
-pm2 delete ____
-pm2 start _____
+pm2 delete MM2SitesServer.js
+pm2 start MM2SitesServer
 echo refresh complete!
 echo ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
